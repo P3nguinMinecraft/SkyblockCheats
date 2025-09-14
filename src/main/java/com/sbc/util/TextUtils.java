012f3428ee1f73ce1bff967b83222fc169f0b620
penguin <penguin586970@gmail.com>
2025-09-14 02:08:40 -0400

    formatting

--- src/main/java/com/sbc/util/TextUtils.java
+++ src/main/java/com/sbc/util/TextUtils.java
@@ -6,7 +6,10 @@ import com.mojang.serialization.JsonOps;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.text.TextCodecs;
+import net.minecraft.util.Formatting;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Optional;
 
 public class TextUtils {
     public static String toString(Text text){
@@ -45,4 +48,34 @@ public class TextUtils {
         return extra2.has("color") ? extra2.get("color").getAsString() : null;
     }
 
+
+    public static String getFormattedText(Text text) {
+        StringBuilder sb = new StringBuilder();
+
+        text.visit((style, content) -> {
+            sb.append(serializeFormattingToString(style));
+            sb.append(content);
+            sb.append("§r");
+            return Optional.empty();
+        }, Style.EMPTY);
+
+        return sb.toString();
+    }
+
+    public static String serializeFormattingToString(Style style) {
+        StringBuilder sb = new StringBuilder();
+        if (style.getColor() != null) {
+            Formatting formatting = Formatting.byName(style.getColor().getName());
+            if (formatting != null) {
+                sb.append(formatting);
+            }
+        }
+        if (style.isBold()) sb.append("§l");
+        if (style.isItalic()) sb.append("§o");
+        if (style.isUnderlined()) sb.append("§n");
+        if (style.isObfuscated()) sb.append("§k");
+        if (style.isStrikethrough()) sb.append("§m");
+
+        return sb.toString();
+    }
 }
