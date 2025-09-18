98308d7470cc93f639aeae4220692a5778f261d1
penguin <penguin586970@gmail.com>
2025-09-18 10:57:29 -0400

    fishing rod mixin

---
+++ src/main/java/com/sbc/mixin/FishingRodItemMixin.java
@@ -0,0 +1,20 @@
+package com.sbc.mixin;
+
+import com.sbc.feature.fishing.TrophyMacro;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.FishingRodItem;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(FishingRodItem.class)
+public class FishingRodItemMixin {
+    @Inject(method = "use", at = @At("HEAD"))
+    private static void onUse(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<ActionResult> cir){
+        TrophyMacro.rodCast(world, user, hand);
+    }
+}
